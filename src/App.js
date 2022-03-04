38a5a6881267d8858fc5c2e0f05ee27d4cffc8fb
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-03-04 12:54:54 +0000

    cleaned up code and added tooltip

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
   return (
     <div className="App">
         <h1>Under Construction</h1>
-        <img src="/construction_img.jpeg" />
-        <img src="" alt="" />
+       <div class="construction-img">
+       <a href="https://unsplash.com/photos/3yoTPuYR9ZY"><img src="/construction_img.jpeg" alt="Sillhoette of construction workers and a construction site in the evening"/> </a>
+          <div class="tooltip">
+              <span class="tooltiptext">Image courtesy of Shivendu Shukla</span>
+          </div>
+       </div>
     </div>
   );
 }
